48d618baf677ebdab08f01d6d926e682d24d6132
Matt Aimonetti <mattaimonetti@gmail.com>
2013-02-26 10:04:57 -0800

    Ruby 1.8 doens't support <symbol>.upcase so cast to a string if needed

--- lib/fire/request.rb
+++ lib/fire/request.rb
@@ -23,7 +23,7 @@ module FireAndForget
     def execute
       uri = URI.parse(url)
       req = []
-      req << "#{method.upcase} #{uri.request_uri} HTTP/1.0"
+      req << "#{method.respond_to?(:upcase) ? method.upcase : method.to_s.upcase} #{uri.request_uri} HTTP/1.0"
       req << "Host: #{uri.host}:#{uri.port}"
       req << "Content-Type: #{content_type}"
       req << "Content-Length: #{body_length}"
